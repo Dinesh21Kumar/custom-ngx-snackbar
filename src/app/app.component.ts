e2a43fdf2d20017c2a3f7b1ab43824bbc5287fd7
Dinesh Kumar <dinesh21.kumar@ril.com>
2019-08-28 11:58:13 +0530

    change app html and ts logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'custom-snackbar';
+  showError:boolean = false;
+  userList: any = [];
+  errorList: any = [];
+  errorObj: any = {"heading":"Error Message Heading...","desc":"this error occured due to xyz reasons!!"}
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    console.log("main component ng on it");
+    this.http.get("http://jsonplaceholder.typicode.com/users").
+      subscribe(
+        (data) => {
+        console.log(data)
+        this.userList = data;
+        },
+        (err) => {
+          console.log(err);
+          this.errorObj["heading"] = err.name;
+          this.errorObj["desc"]= err.message;
+          console.log(this.errorObj);
+          this.errorList.push(this.errorObj);
+          this.showError = true;
+        }
+    )
+  }
+  
+  
+
+  // getData(e) {
+  //   console.log(e);
+  //   this.showError = e["showError"];
+  // }
 }
